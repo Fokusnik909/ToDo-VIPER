ef78014507c0dd64fd7a51a874ae786f8155371b
Arthur <Fokusnik90999@gmail.com>
2025-08-08 00:20:45 +0500

    del storyboard

--- ToDo VIPER/Base/SceneDelegate.swift
+++ ToDo VIPER/Base/SceneDelegate.swift
@@ -13,10 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
         let tasksListVC = TasksListModuleBuilder.build()
         let nav = UINavigationController(rootViewController: tasksListVC)
-        window?.rootViewController = nav
-        window?.makeKeyAndVisible()
+        
+        window.rootViewController = nav
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
